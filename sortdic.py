7c7db6dcc6acbf410a574785e1ebfdb879dbd980
athulanil17 <118271019+athulanil17@users.noreply.github.com>
2023-01-09 17:09:07 +0530

    Update sortdic.py

--- sortdic.py
+++ sortdic.py
@@ -2,14 +2,4 @@ dic1={1:10, 2:20}
 dic2={3:30, 4:40}
 dic3={5:50,6:60}
 dict_num=dic1|dic2|dic3
-
-
-
-for key in dict_num:
-    print("key:", key)
-
-for value in dict_num.values():
-    print("value:", value)
-
-for item in dict_num.items():
-    print("Item:", item)
+print(dict_num)
