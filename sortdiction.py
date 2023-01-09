8b3be0f3a3a422affe4118a9a79ead908a4de7da
athulanil17 <118271019+athulanil17@users.noreply.github.com>
2023-01-09 17:15:20 +0530

    Add files via upload

---
+++ sortdiction.py
@@ -0,0 +1,9 @@
+dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+for key in dict_num:
+    print("key:", key)
+
+for value in dict_num.values():
+    print("value:", value)
+
+for item in dict_num.items():
+    print("Item:", item)
